5da71939e3871c145cd09697b6ce58642256c99e
Phil Diegmann <diegmann@blueworld-gmbh.de>
2017-02-01 08:38:09 +0100

    buffer incompatability

--- ReceptionServer.js
+++ ReceptionServer.js
@@ -29,13 +29,12 @@ var server = http.createServer(function(request, response) {
 	var headers = request.headers;
  	var method = request.method;
   	var url = request.url;
-  	var body = [];
+  	var body = '';
 	request.on('error', function(err) {
 		console.error(err);
 	}).on('data', function(chunk) {
-		body.push(chunk);
+		body += chunk;
 	}).on('end', function() {
-		body = Buffer.concat(body).toString();
 		response.on('error', function(err) {
 			console.error(err);
 		});
